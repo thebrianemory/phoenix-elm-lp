19bd55aa23273cfab66093710c06ff5cc2b2d47e
thebrianemory <the@brianemory.com>
2017-12-06 22:30:58 -0800

    Generate marketing lead context

---
+++ landing_page/lib/landing_page/marketing/lead.ex
@@ -0,0 +1,22 @@
+defmodule LandingPage.Marketing.Lead do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias LandingPage.Marketing.Lead
+
+  @derive {Poison.Encoder, only: [:full_name, :email]}
+
+  schema "leads" do
+    field :email, :string
+    field :full_name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(%Lead{} = lead, attrs) do
+    lead
+    |> cast(attrs, [:full_name, :email])
+    |> validate_required([:full_name, :email])
+    |> unique_constraint(:email)
+  end
+end
